dd3a048ba6514e490a01eceb863e95f5ed5a0725
Levina Shaharani P <121291219+Levinasp-bot@users.noreply.github.com>
2023-11-05 11:00:00 +0700

    Create Customer.php

---
+++ src/marketing_service/app/Models/Customer.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Customer extends Model
+{
+    protected $primaryKey = 'id_customer'; // Atur primary key model
+    protected $fillable = ['id_customer', 'name', 'email_cust', 'no_telp', 'sex', 'precise_location','id_kelurahan', 'created_at', 'updated_at'];
+
+    // Atur tipe data kolom
+    protected $casts = [
+        'created_at' => 'datetime:Y-m-d H:i:s',
+        'updated_at' => 'datetime:Y-m-d H:i:s',
+    ];
+}
