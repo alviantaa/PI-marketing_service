f39fde11660f0d47695350fe963bb14da2d5a0f2
Hanifah Rahmajati <hanrahmajati@gmail.com>
2023-11-11 10:19:15 +0700

    Add Consum Report & Auto Order (not sure bout AO)

---
+++ src/marketing_service/app/Models/ConsumptionReport.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ConsumptionReport extends Model
+{
+    protected $primaryKey = 'id_report';
+    protected $fillable = [
+        'id_report','id_order', 'id_customer', 'consumption', 'remaining_product'
+    ];
+	protected $casts = [
+	    'created_at' => 'datetime:Y-m-d H:i:s', 
+	    'updated_at' => 'datetime:Y-m-d H:i:s',
+	]; 
+
+    public function order()
+    {
+        return $this->belongsTo(Order::class, 'id_order', 'id_order');
+    }
+    
+    public function customer()
+    {
+        return $this->belongsTo(Customer::class, 'id_customer', 'id_customer');
+    }
+}
