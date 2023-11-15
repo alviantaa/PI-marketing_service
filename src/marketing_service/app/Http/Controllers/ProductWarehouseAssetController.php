7962e569582176a6075dff1f870598bd99affa11
Tikta Shapa Vania Adristi <88105466+tiktaa@users.noreply.github.com>
2023-11-15 14:35:16 +0700

    Update ProductWarehouseAssetController.php
    edit controller asset

--- src/marketing_service/app/Http/Controllers/ProductWarehouseAssetController.php
+++ src/marketing_service/app/Http/Controllers/ProductWarehouseAssetController.php
@@ -2,10 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\ProductLocation;
 use App\Models\Product;
 use App\Models\Warehouse;
+use App\Models\Asset;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+
 
 class ProductWarehouseAssetController extends Controller
 {
@@ -32,7 +35,7 @@ class ProductWarehouseAssetController extends Controller
     {
         $validator = Validator::make($request->all(), [
             'id_level' => 'required|exists:level,id_level',
-            'PIC' => 'required|exists:employee,id_employee',
+            'PIC' => 'required|max:6|exists:employee,id_employee',
             'name' => 'required|string|max:100',
             'capacity' => 'required|numeric',
             'address' => 'required|string|max:200',
@@ -46,4 +49,41 @@ class ProductWarehouseAssetController extends Controller
 
         return response()->json($warehouse, 201);
     }
+    public function storeProductLocation(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'id_product_location' => 'required|max:6|unique:product_location',
+            'id_product' => 'required|max:6|exists:product,id_product',
+            'id_warehouse' => 'required|max:6|exists:warehouse,id_warehouse',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 400);
+        }
+
+        $productlocation = ProductLocation::create($request->all());
+
+        return response()->json($productlocation, 201);
+    }
+
+public function storeAsset(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'id_asset' => 'required|max:6|unique:asset',
+	        'asset_name' => 'required|string|max:100',
+            'asset_type' => 'required|string|max:100',
+	        'spesification' => 'required|string|max:245',
+            'PIC' => 'required|string|max:6|exists:employee,id_employee',
+            'id_warehouse' => 'required|max:6|exists:warehouse,id_warehouse',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 400);
+        }
+
+        $asset = Asset::create($request->all());
+
+        return response()->json($asset, 201);
+    }
+
 }
