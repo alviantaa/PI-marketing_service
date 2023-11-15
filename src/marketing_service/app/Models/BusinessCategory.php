958d96336699b84718ada46725c171e7cb733f72
Levina Shaharani P <121291219+Levinasp-bot@users.noreply.github.com>
2023-11-15 14:54:37 +0700

    Update BusinessCategory.php

--- src/marketing_service/app/Models/BusinessCategory.php
+++ src/marketing_service/app/Models/BusinessCategory.php
@@ -6,8 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class BusinessCategory extends Model
 {
+    protected $table = 'business_categories';
     protected $primaryKey = 'id_business_category';
-    protected $fillable = ['id_business_category', 'category', 'max_capacity', 'id_product', 'description', 'created_at', 'updated_at'];
+    protected $fillable = ['id_business_category', 'id_product', 'category', 'max_capacity', 'description', 'created_at', 'updated_at'];
 
     public function product()
     {
