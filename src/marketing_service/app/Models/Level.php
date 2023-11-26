7f387b00f35f7c6180278b75fb92351e76f7e42f
Levina Shaharani P <121291219+Levinasp-bot@users.noreply.github.com>
2023-11-26 17:38:06 +0700

    Create Level.php

---
+++ src/marketing_service/app/Models/Level.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Level extends Model
+{
+    protected $table = 'level';
+    protected $primaryKey = 'id_level';
+    protected $fillable = ['level'];
+    public $timestamps = true;
+}
