96b38fb253b2563d3f08b9d966dfee024e4e0a63
Levina Shaharani P <121291219+Levinasp-bot@users.noreply.github.com>
2023-11-26 17:37:12 +0700

    Update api.php

--- src/marketing_service/routes/api.php
+++ src/marketing_service/routes/api.php
@@ -30,17 +30,25 @@ Route::group(['prefix' => 'marketing'], function ($router) {
 
     //PRODUK, WAREHOUSE, and ASSETs
     Route::post('/add-product', [ProductWarehouseAssetController::class, 'addProduct']);
+    Route::get('/get-product', [ProductWarehouseAssetController::class, 'readProduct']);
     Route::post('/add-warehouse', [ProductWarehouseAssetController::class, 'addWarehouse']);
+    Route::get('/get-warehouse', [ProductWarehouseAssetController::class, 'readWarehouse']);
     Route::post('/add-product-location', [ProductWarehouseAssetController::class, 'storeProductLocation']);
+    Route::get('/get-product-location', [ProductWarehouseAssetController::class, 'readProductLocation']);
     Route::post('/add-asset', [ProductWarehouseAssetController::class, 'storeAsset']);
+    Route::get('/get-asset', [ProductWarehouseAssetController::class, 'readAsset']);
 
     //CUSTOMER and BUSINESS CATEGORY
     Route::post('/add-customer', [CustomerBusinessCategoryController::class, 'storeCustomer']);
     Route::get('/get-customer', [CustomerBusinessCategoryController::class, 'readCustomer']);
     Route::post('/add-business-category', [CustomerBusinessCategoryController::class, 'storeBusinessCategory']);
+    Route::get('/get-business-category', [CustomerBusinessCategoryController::class, 'readBusinessCategory']);
 
     //ORDER and REPORT
     Route::post('/add-consumption-report', [OrderReportController::class, 'storeConsumptionReport']);
-    Route::post('/customer-order', [OrderReportController::class, 'p']);
+    Route::get('/get-consumption-report', [OrderReportController::class, 'readConsumptionReport']);
+    Route::post('/customer-order', [OrderReportController::class, 'storeAutoOrder']);
+    Route::post('/get-customer-order', [OrderReportController::class, 'readAutoOrder']);
     Route::post('/marketing-order', [OrderReportController::class, 'addMarketingOrder']);
+    Route::post('/get-marketing-order', [OrderReportController::class, 'readMarketingOrder']);
 });
